946066c1f9ccc5c33e0dc8a17db57ce24e4642f4
Victor Pagnozi <victor.pagnozi@gmail.com>
2022-11-08 22:05:25 -0300

    Lintter

--- src/server.js
+++ src/server.js
@@ -1,61 +1,57 @@
-const express = require('express')
-const ejs = require('ejs')
-const path = require('path')
-const puppeteer = require('puppeteer')
-const app = express()
+const express = require("express");
+const ejs = require("ejs");
+const path = require("path");
+const puppeteer = require("puppeteer");
+const app = express();
 
 const passengers = [
-    {
-        name: "Joyce",
-        flightNumber: 7859,
-        time: "18h00",
-    },
-    {
-        name: "Brock",
-        flightNumber: 7859,
-        time: "18h00",
-    },
-    {
-        name: "Eve",
-        flightNumber: 7859,
-        time: "18h00",
-    },
+  {
+    name: "Joyce",
+    flightNumber: 7859,
+    time: "18h00",
+  },
+  {
+    name: "Brock",
+    flightNumber: 7859,
+    time: "18h00",
+  },
+  {
+    name: "Eve",
+    flightNumber: 7859,
+    time: "18h00",
+  },
 ];
 
-app.get('/pdf', async(request, response) => {
+app.get("/pdf", async (request, response) => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
 
-    const browser = await puppeteer.launch()
-    const page = await browser.newPage()
+  await page.goto("http://localhost:3000/", {
+    waitUntil: "networkidle0",
+  });
 
-    await page.goto('http://localhost:3000/', {
-        waitUntil: 'networkidle0'
-    })
+  const pdf = await page.pdf({
+    printBackground: true,
+    format: "Letter",
+  });
 
-    const pdf = await page.pdf({
-        printBackground: true,
-        format: 'Letter'
-    })
+  await browser.close();
 
-    await browser.close()
+  response.contentType("application/pdf");
 
-    response.contentType("application/pdf")
+  return response.send(pdf);
+});
 
-    return response.send(pdf)
+app.get("/", (request, response) => {
+  const filePath = path.join(__dirname, "print.ejs");
+  ejs.renderFile(filePath, { passengers }, (err, html) => {
+    if (err) {
+      return response.send("Erro na leitura do arquivo");
+    }
 
-})
+    // enviar para o navegador
+    return response.send(html);
+  });
+});
 
-app.get('/', (request, response) => {
-
-    const filePath = path.join(__dirname, "print.ejs")
-    ejs.renderFile(filePath, { passengers }, (err, html) => {
-        if(err) {
-            return response.send('Erro na leitura do arquivo')
-        }
-    
-        // enviar para o navegador
-        return response.send(html)
-    })
-   
-})
-
-app.listen(3000)
+app.listen(3000);
